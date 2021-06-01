3d45204777172cbc15171e61143ea08669f0692a
Jihad Dabat <jihad.abu-dabat@extern.dkbcodefactory.com>
2021-06-01 13:58:04 +0200

    added annotation and dependencies

--- src/main/kotlin/com/senacor/ci/simpleprovider/SimpleProviderApp.kt
+++ src/main/kotlin/com/senacor/ci/simpleprovider/SimpleProviderApp.kt
@@ -1,8 +1,13 @@
 package com.senacor.ci.simpleprovider
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition
+import org.eclipse.microprofile.openapi.annotations.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@OpenAPIDefinition(
+	info = Info(title = "Some title", description = "Some other description", version = "v0.0.1")
+)
 @SpringBootApplication
 class SimpleProviderApp
 
--- src/main/kotlin/com/senacor/ci/simpleprovider/model/Item.kt
+++ src/main/kotlin/com/senacor/ci/simpleprovider/model/Item.kt
@@ -1,9 +1,5 @@
 package com.senacor.ci.simpleprovider.model
-
 import java.math.BigDecimal
+import javax.validation.constraints.NotBlank
 
-@Schema(
-    name = "Item",
-    description = "Data Object to be returned after a search",
-    oneOf = com.senacor.ci.simpleprovider.model.Item.class)
-data class Item(val name: String, val price: BigDecimal, val available: Int)
+data class Item(@NotBlank val name: String, @NotBlank val price: BigDecimal, @NotBlank val available: Int)
